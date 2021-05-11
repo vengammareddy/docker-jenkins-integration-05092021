18e8308c2513ee1bebed56aa04d9708cdfd4b956
MUTTURI VENGAMMA <vengamma4@gmail.com>
2021-05-11 11:29:55 +0530

    java first commit.

---
+++ src/main/java/Testdevops.java
@@ -0,0 +1,6 @@
+public class Testdevops {
+    public static void main(String[] args) {
+        System.out.println("Hello..");
+        System.out.println("welcome to git and jenkins integration..");
+    }
+}
