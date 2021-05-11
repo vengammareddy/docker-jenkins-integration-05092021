10db9374957299b5fbe558b22e5b20c204602808
MUTTURI VENGAMMA <vengamma4@gmail.com>
2021-05-11 12:11:36 +0530

    second commit done

--- src/main/java/Testdevops.java
+++ src/main/java/Testdevops.java
@@ -1,6 +1,10 @@
 public class Testdevops {
     public static void main(String[] args) {
-        System.out.println("Hello..");
-        System.out.println("welcome to git and jenkins integration..");
+        System.out.println("Hello1..");
+        System.out.println("Welcome to github and jenkins integration1..");
+        System.out.println("Hello2..");
+        System.out.println("Welcome to github and jenkins integration2..");
+        System.out.println("Hello3..");
+        System.out.println("Welcome to github and jenkins integration3..");
     }
 }
